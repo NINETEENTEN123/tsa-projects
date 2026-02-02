c55fb21b1a6786d6d22450903006c8d5a0a22376
NINETEENTEN123 <nineteenten123@gmail.com>
2026-02-02 17:43:17 -0500

    Update character names and text positioning

--- video-game/textures.py
+++ video-game/textures.py
@@ -14,11 +14,11 @@ class mainScreen:
     font = pygame.font.SysFont(None, 48)
     text_surface = font.render("Choose your character", True, BLACK)
     text_rect = text_surface.get_rect()
-    text_rect.center = (400 / 2, (350 / 2) - 80)
+    text_rect.center = (400 / 2, (350 / 2) - 120)
     
 class brutusName:
     font = pygame.font.SysFont(None, 24)
-    text_surface = font.render("Brutus", True, BLACK)
+    text_surface = font.render("Axiom", True, BLACK)
     text_rect = text_surface.get_rect()
     text_rect.center = ((400 / 2) - 140, (350 / 2) - 40)
     
@@ -30,12 +30,12 @@ class metaName:
     
 class zippyName:
     font = pygame.font.SysFont(None, 24)
-    text_surface = font.render("Zippy", True, BLACK)
+    text_surface = font.render("Nexus", True, BLACK)
     text_rect = text_surface.get_rect()
     text_rect.center = ((400 / 2) -50, (350 / 2) - 40)
     
 class vigneshName:
     font = pygame.font.SysFont(None, 24)
-    text_surface = font.render("Vignesh", True, BLACK)
+    text_surface = font.render("Hydra", True, BLACK)
     text_rect = text_surface.get_rect()
     text_rect.center = ((400 / 2) +50, (350 / 2) - 40) 
