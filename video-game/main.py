1333f57118315f834519c2f76376c18feac38929
NINETEENTEN123 <nineteenten123@gmail.com>
2026-02-02 17:42:23 -0500

    Add button and image loading functionality

--- video-game/main.py
+++ video-game/main.py
@@ -5,6 +5,9 @@ import sys
 import json
 import io
 import urllib.request
+import os
+import pygame_widgets
+from pygame_widgets.button import Button
 
 # Other files / classes
 
@@ -18,6 +21,11 @@ import functions
 # Variables
 
 image1_url = "https://codehs.com/uploads/8cb18d6cb1b15e64ecab69317625acde"
+image2_url = "https://codehs.com/uploads/e76c5e7f981adac1a62c5e5a9cf30c78"
+butt1_url = "https://codehs.com/uploads/fff3a7d81e49ad661c57946a5a704e75"
+
+    #                   Screen var                          X   Y    Width Height  Text             Function
+char1_button = Button(pygame.display.set_mode((450, 350)), 35, 70, 50, 50, text='▶️', onClick=lambda: print('Clicked!'))
 
 # import later (scene 1)
 
@@ -30,7 +38,7 @@ clock = pygame.time.Clock()
 running = True
 
 while running:
-    for event in pygame.event.get(): # stops running function
+    for event in pygame.event.get(): # Stops the game
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
@@ -57,9 +65,19 @@ while running:
     
         # Load image 1
         
-    online_image = functions.loadImage(image1_url)
-    sized_image = pygame.transform.scale(online_image, (50, 180))
-    screen.blit(sized_image, (30, 150))
+    online_image = functions.loadImage(image1_url)      # Size dimensions
+    sized_image = pygame.transform.scale(online_image, (50, 180)) # Variable used to scale image
+    screen.blit(sized_image, (30, 150)) # Coordinates to position image
+    
+        # Load Vignesh Image
+    
+    online_image3 = functions.loadImage(image2_url)
+    sized_image3 = pygame.transform.scale(online_image3, (50, 180))
+    screen.blit(sized_image3, (220, 150))
+    
+        # Load Play Button 1
+        
+    pygame_widgets.update(pygame.event.get())
     
     # Flip display
     
